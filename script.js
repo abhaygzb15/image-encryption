5c9e31cfb187be1fa384799d04ae5ebbb66af928
Mohit <128162892+katyalmohit@users.noreply.github.com>
2023-09-09 16:30:12 +0530

    Add button and resolve conflicts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById('uploadButton').addEventListener('click', function() {
-    document.getElementById('imageUpload').click();
+document.getElementById("uploadButton").addEventListener("click", function () {
+  document.getElementById("imageUpload").click();
 });
 
 // You can add more JavaScript functionality here if needed
@@ -7,28 +7,32 @@ document.getElementById('uploadButton').addEventListener('click', function() {
 
 // Function to handle image selection and display
 function handleImageUpload(event) {
-    const selectedImage = document.getElementById('selectedImage');
-    const fileInput = event.target;
+  const selectedImage = document.getElementById("selectedImage");
+  const fileInput = event.target;
 
-    if (fileInput.files.length > 0) {
-        const selectedFile = fileInput.files[0];
+  if (fileInput.files.length > 0) {
+    const selectedFile = fileInput.files[0];
 
-        // Check if the selected file is an image
-        if (selectedFile.type.startsWith('image/')) {
-            const imageUrl = URL.createObjectURL(selectedFile);
-            selectedImage.src = imageUrl;
+    // Check if the selected file is an image
+    if (selectedFile.type.startsWith("image/")) {
+      const imageUrl = URL.createObjectURL(selectedFile);
+      selectedImage.src = imageUrl;
 
-            // Make the selected image visible
-            selectedImage.style.display = 'block';
+      // Make the selected image visible
+      selectedImage.style.display = "block";
 
-            // You can adjust the CSS properties of the selected image container here
-            const selectedImageContainer = document.querySelector('.selected-image-container');
-            selectedImageContainer.style.display = 'block';
-        } else {
-            alert('Please select a valid image file.');
-        }
+      // You can adjust the CSS properties of the selected image container here
+      const selectedImageContainer = document.querySelector(
+        ".selected-image-container"
+      );
+      selectedImageContainer.style.display = "block";
+    } else {
+      alert("Please select a valid image file.");
     }
+  }
 }
 
 // Attach the handleImageUpload function to the file input change event
-document.getElementById('imageUpload').addEventListener('change', handleImageUpload);
+document
+  .getElementById("imageUpload")
+  .addEventListener("change", handleImageUpload);
